5bcaff85ccc42832507ac724875d00e327100d3d
garipovagz <garipova.gz@mail.ru>
2024-06-14 15:11:42 +0300

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/characters/',
+  },
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@vee-validate/nuxt', '@vueuse/nuxt'],
   css: ['~/assets/css/main.css'],
